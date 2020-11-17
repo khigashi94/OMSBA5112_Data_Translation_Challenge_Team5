0cfbf007bda4bb8843ffd337f3308b38ac72e3d4
Lynnadtran <ltran3@seattleu.edu>
2020-11-17 00:32:36 -0800

    added the key for joining later

--- Lynna - Data Translation Code/Lynna_DataTranslation_Code.R
+++ Lynna - Data Translation Code/Lynna_DataTranslation_Code.R
@@ -12,12 +12,17 @@ household <- read_dta('sec1.dta')
 #education of father, education of mother
 
 father_ed <- household %>%          #levels of education of individuals father
-  select(nh, pid,  s1q15)  %>%
+  select(clust, nh, pid,  s1q15)  %>%
   filter(!is.na(s1q15))
 
+father_ed$new_PID <- paste(father_ed$clust, father_ed$nh, father_ed$pid, sep = "_" )
+
+
 mother_ed <- household %>%
-  select(nh, pid,  s1q19)  %>%
+  select(clust, nh, pid,  s1q19)  %>%
   filter(!is.na(s1q19))
+
+mother_ed$new_PID <- paste(mother_ed$clust, mother_ed$nh, mother_ed$pid, sep = "_" )
 
 
 ed_general <- read_dta('sec2a.dta')
@@ -25,8 +30,11 @@ ed_general <- read_dta('sec2a.dta')
 
 ##level_study -- #highest level of study completed and highest qualification attained
 level_study <- ed_general %>%         #highest level of study completed and highest qualification attained
-  select(nh, pid,  s2aq2, s2aq3)  %>%
+  select(clust,nh, pid,  s2aq2, s2aq3)  %>%
   filter(!is.na(s2aq3))
+
+level_study$new_PID <- paste(level_study$clust, level_study$nh, level_study$pid, sep = "_" )
+
 
 ##time going to school 
 time_spent_going_school <- ed_general%>%  #time spend going to school *will have to combine hours and minutes into one variable*
@@ -38,52 +46,73 @@ time_spent_going_school_combined <- ed_general%>%  #time spend going to school *
 
 ##Total educational expenses
 educ_expenses <- ed_general%>%
-  select(nh, pid,  s2aq6, s2aq7, s2aq8, s2aq9, s2aq10, s2aq11, s2aq12, s2aq13, s2aq13a)
+  select(clust, nh, pid,  s2aq6, s2aq7, s2aq8, s2aq9, s2aq10, s2aq11, s2aq12, s2aq13, s2aq13a)
 
 educ_expenses[is.na(educ_expenses)] <- 0
 
 educ_expenses_totals <- educ_expenses %>%
   mutate(rowSums(educ_expenses))  %>%
-  mutate(totals = rowSums(educ_expenses) - (nh + pid)) %>%
-  select(nh, pid, totals)
+  mutate(totals = rowSums(educ_expenses) - (nh + pid + clust)) %>%
+  select(clust, nh, pid, totals)
 
-##trying to get the sum of school expenses 
-sum_education_expenses2 <- educ_expenses  %>% 
-  filter(!is.na( s2aq6)) %>% 
-  mutate(cumsum(educ_expenses))## na.rm = TRUE)#, dims = 1))
-                
+educ_expenses_totals$new_PID <- paste(educ_expenses_totals$clust, educ_expenses_totals$nh, educ_expenses_totals$pid, sep = "_" )
+
 
 scholarship <- ed_general%>%
-  select(nh, pid,  s2aq15, s2aq16)
+  filter(s2aq15 == 1) %>%
+  select(clust, nh, pid,  s2aq15, s2aq16)
 
-#variable might not be needed 
-ed_career <- read_dta('sec2b.dta')
-#higher levels of education 
+scholarship$new_PID <- paste(scholarship $clust, scholarship$nh, scholarship$pid, sep = "_" )
+
 
 ##Education: Literacy/Apprenticeship
 ed_literacy <- read_dta('sec2c.dta')
 
 #can read in either Ghanaian or English language
 reading_literacy <- ed_literacy %>%
-  select(nh, pid, s2cq1, s2cq2 )
+  filter(s2cq2 != 1 | s2cq1 == 1) %>%
+  select(clust, nh, pid, s2cq1, s2cq2 )
+
+reading_literacy$new_PID <- paste(reading_literacy$clust, reading_literacy$nh, reading_literacy$pid, sep = "_" )
+
 
 #can read in English
 read_english <- ed_literacy %>%
-  select(nh, pid, s2cq1 )
+  filter(s2cq1 == 1) %>%
+  select(clust, nh, pid, s2cq1 )
+
+read_english$new_PID <- paste(read_english$clust, read_english$nh, read_english$pid, sep = "_" )
+
 
 #can read in Ghana
 read_ghana <- ed_literacy %>%
-  select(nh, pid, s2cq2 )
+  filter(s2cq2 != 1 ) %>%
+  select(clust, nh, pid, s2cq2 )
+
+read_ghana$new_PID <- paste(read_ghana$clust, read_ghana$nh, read_ghana$pid, sep = "_" )
+
 
 #can write in either Ghanaian or English language 
 writing_literacy <- ed_literacy %>%
-  select(nh, pid, s2cq3, s2cq4)
+  filter(s2cq3 == 1 | s2cq4 != 1) %>%
+  select(clust, nh, pid, s2cq3, s2cq4)
+
+writing_literacy$new_PID <- paste(writing_literacy$clust, writing_literacy$nh, writing_literacy$pid, sep = "_" )
 
 #can write in English language 
-write_ghana <- ed_literacy %>%
-  select(nh, pid, s2cq3 )
+write_english<- ed_literacy %>%
+  filter(s2cq3 == 1) %>%
+  select(clust, nh, pid, s2cq3 )
+
+write_english$new_PID <- paste(write_english$clust, write_english$nh, write_english$pid, sep = "_" )
+
 
 ##can write in Ghana language 
 write_ghana <- ed_literacy %>%
-  select(nh, pid, s2cq4 )
+  filter(s2cq4 != 1) %>%
+  select(clust, nh, pid, s2cq4 )
+
+
+write_ghana$new_PID <- paste(write_ghana$clust, write_ghana$nh, write_ghana$pid, sep = "_" )
+
 
