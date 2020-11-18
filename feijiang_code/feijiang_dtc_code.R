33252bf813f3452b996ecf0fdfe714bfe258ed3b
fjiang24 <71153493+fjiang24@users.noreply.github.com>
2020-11-17 20:48:03 -0800

    converted ropes to acres

--- feijiang_code/feijiang_dtc_code.R
+++ feijiang_code/feijiang_dtc_code.R
@@ -10,52 +10,54 @@ library(tidyverse)
 library(haven)
 library(ggplot2)
 library(dplyr)
+library(magrittr)
 
 
 ### ------ agriculture land ------ ###
 agri_land <- read_dta('data/glss4_new/sec8a1.dta')
-agri_land$new_pid <- paste(agri_land$clust, agri_land$nh, sep="_") 
-land_info <- select(agri_land, nh, clust, s8aq3, s8aq4) %>% 
-  filter(!is.na(s8aq3)) %>% 
-  filter(!is.na(s8aq4))
+agri_land$new_hhid <- paste(agri_land$clust, agri_land$nh, sep="_") 
+land <- select(agri_land, nh, clust, s8aq2, s8aq3, s8aq4, s8aq5)  
 
 # variables
-plot_areas <- land_info$s8aq3      # unit of measure in acres, poles, ropes, other
-land_owned <- land_info$s8aq4
+hh_own_any_lands_12_months <- land$s8aq2 
+plot_areas <- land$s8aq3        # unit of measure in acres, poles, ropes, other
+land_owned <- land$s8aq4
+was_land_bought_rented <- land$s8aq5
+
+land_ropes_to_acres <- land %>%  
+  select(clust, nh, s8aq2, s8aq3, s8aq4, s8aq5) %>%
+  filter(s8aq3 == 3) %>%
+  mutate(plot_total_size_in_acres = (s8aq4 * 0.1111))
 
 # visualize by plot area
-ggplot(data = agri_land, mapping = aes (x = s8aq3, y = nh)) + 
+ggplot(data = agri_land, mapping = aes (x = s8aq4, y = nh)) + 
         geom_point(na.rm = TRUE)
-#  geom_bar(mapping = aes(x = s8aq3)) 
-#  geom_histogram(mapping = aes (x = s8aq4))
-        
 
 ### ------ agriculture plot details ------ ###
 agri_plot <- read_dta('data/glss4_new/sec8b.dta') 
-agri_plot$new_pid <- paste(agri_plot$clust, agri_plot$nh, sep="_") 
-plot_info <- select(agri_plot, nh, clust, s8bq4a, s8bq4b, s8bq5, s8bq12a, s8bq12b, s8bq13a, s8bq13b, s8bq13c, s8bq13d)
+agri_plot$new_hhid <- paste(agri_plot$clust, agri_plot$nh, sep="_") 
+plot <- select(agri_plot, nh, clust, s8bq4a, s8bq4b, s8bq5, s8bq12a, s8bq12b)
 
 # variables
-land_size <- plot_info$s8bq4a
-unit_of_measure <- plot_info$s8bq4b    # unit of measure in acres, poles, ropes, other
-farm_owned <- plot_info$s8bq5
-crop1_growing <- plot_info$s8bq12a
-crop2_growing <- plot_info$s8bq12b
-crop_code1 <- plot_info$s8bq13a
-crop_code2 <- plot_info$s8bq13b
-crop_code3 <- plot_info$s8bq13c
-crop_code4 <- plot_info$s8bq13d
+land_size <- plot$s8bq4a
+unit_of_measure <- plot$s8bq4b    # unit of measure in acres, poles, ropes, other
+farm_owned <- plot$s8bq5
+crop1_growing <- plot$s8bq12a
+crop2_growing <- plot$s8bq12b
 
-# convert ropes to acres -- office hour
-x <-  s8bq4b
-if (x == 3) {
-  "s8bq4b * .1111"
-}  
-
+# convert ropes to acres -- office houpr
+plot_ropes_to_acres <- plot %>%  
+select(clust, nh, s8bq4a, s8bq4b, s8bq5, s8bq12a, s8bq12b) %>%
+  filter(s8bq4b == 3) %>%
+  mutate(plot_total_size_in_acres = (s8bq4a * 0.1111))
 
 ### ----- merge agri land and plot by nh and clust. filter N/A ----- ###
-agri_merge <- full_join(agri_land, agri_plot,  by = "nh", "clust", all.y = TRUE) %>% 
-  select(nh, clust.x, s8aq3, s8aq4, s8bq4a, s8bq4b, s8bq5, s8bq12a, s8bq12b, s8bq13a, s8bq13b, s8bq13c, s8bq13d) %>% 
+agri_merge <- left_join(land, total_size_in_acres, by = "nh", "clust", all.y = TRUE) %>% 
+  select(nh, clust.x, s8aq3, s8aq4, s8bq4a, s8bq4b, s8bq5, s8bq12a, s8bq12b)
+
+
+agri_merge_2_acres <- left_join(agri_merge, ropes_to_acres, by = "nh", "clust", all.y = TRUE) %>% 
+  select(nh, clust, total_size_in_acres, s8aq3, s8aq4, s8bq5, s8bq12a, s8bq12b) %>% 
   filter(!is.na(s8aq3)) 
 
 # visualize land owned by household, legend in land size
